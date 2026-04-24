cd225917f2fe8ba7b27fafc83eade874da43ca33
ROBERTO HENRIQUEZ <henriquez.roberto@gmail.com>
2026-04-24 13:13:06 -0400

    Usar Pooler Supabase

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Usamos SQLite para empezar rápido y fácil
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:xvm8e8Mc2wJ7ZG28@db.rzjhvfhljljkdsvpquwh.supabase.co:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.rzjhvfhljljkdsvpquwh:xvm8e8Mc2wJ7ZG28@aws-1-us-west-2.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
